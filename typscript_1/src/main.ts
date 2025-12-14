1f60355b30086c2785e29cf8c6a0fcf915b83b90
Malik786-git <malikmuhammadjahangir786@gmail.com>
2025-12-15 00:26:53 +0500

    first

---
+++ typscript_1/src/main.ts
@@ -0,0 +1,53 @@
+console.log("Hello world!");
+// variables
+
+let a: number = 10; // a is equal to 10, 10 assign a variable ko.
+console.log(a);
+
+a = 20;
+console.log(a);
+
+let b: boolean = true;
+console.log(b);
+
+let str: string = "Malik M jahangir"; // list of char
+console.log(str);
+
+let ch: string = 'a'; // char type
+console.log(ch);
+
+let x = null;
+console.log(x);
+
+
+// this is comment
+
+/*
+multiline comment
+multiline comment
+multiline comment
+multiline comment
+multiline comment
+*/
+{
+
+  var aaa = "xyz"; // func scope
+}
+console.log(aaa);
+
+
+{
+  const str3 = "Yasir"; // let and const both are block scope
+  console.log(str3);
+}
+
+
+
+
+// let 10firstName = "Malik";
+// let FirstName = "Malik";
+// let first_name = "Malik";
+// console.log(firstName);
+
+
+
