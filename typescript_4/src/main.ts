59a4bd57a02dac210f01c9769e2422ffcc5240d7
Malik786-git <malikmuhammadjahangir786@gmail.com>
2026-01-03 20:44:12 +0500

    code pushed

---
+++ typescript_4/src/main.ts
@@ -0,0 +1,83 @@
+function Logger(n: string) { // Decorator Factory
+  return function (contructor: any) { // Decorator
+    const p = new contructor();
+    p.setName(n)
+    console.log(p.name);
+
+  }
+}
+
+
+
+@Logger("Sarim")
+class Teacher {
+  name = "";
+  constructor() { }
+
+  setName(n: string) {
+    this.name = n
+  }
+}
+
+
+// @Logger()
+// class Student {
+//   name = "";
+//   constructor() { }
+// }
+
+
+
+// Generics
+function merged<U extends object, V extends object>(obj1: U, obj2: V): U & V {
+  return { ...obj1, ...obj2 }
+}
+
+let new_obj = merged({ name: "Sarim" }, { id: 10 });
+console.log(new_obj.name);
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// type guards 
+
+interface Bird {
+  fly: boolean
+}
+
+interface Animal {
+  run: boolean
+}
+
+
+function fun(n: Animal | Bird) {
+  if ("fly" in n) {
+    console.log(n.fly);
+  }
+  if ("run" in n) {
+    console.log(n.run);
+  }
+}
+
+fun({ fly: true } as Bird);
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+type numStr = number | string;
+
+function sum(agr1: number, agr2: number): number;
+function sum(agr1: string, agr2: string): string;
+function sum(agr1: numStr, agr2: numStr) {
+  if (typeof agr1 === 'number' && typeof agr2 === "number") {
+    return agr1 + agr2;
+  } else
+    if (typeof agr1 === 'string' && typeof agr2 === "string") {
+      return agr1 + agr2;
+    } else {
+      return new Error("Invalid Types");
+    }
+}
+
+let val = sum(10, 10);
+let val2 = sum("a", "b");
+
+console.log(val.toFixed(2));
+console.log(val2.charAt(0));
